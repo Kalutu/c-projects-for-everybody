04669df6384b7e32b4c5d88b1b0b7734f53472ab
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-22 22:48:00 +0300

    Update fibonacci.c

--- Fibonacci Series/fibonacci.c
+++ Fibonacci Series/fibonacci.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+void generateFibonacci(int n) {
+    int first = 0, second = 1, next, i;
+
+    printf("Fibonacci Series: ");
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+}
+
+int main() {
+    int terms;
+
+    printf("Enter the number of terms in the Fibonacci series: ");
+    scanf("%d", &terms);
+
+    generateFibonacci(terms);
+
+    return 0;
+}
 
