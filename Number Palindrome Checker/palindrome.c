38c53aff6979662d19f3b7f7edaf7f2c71164399
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-22 22:57:44 +0300

    Update palindrome.c

--- Number Palindrome Checker/palindrome.c
+++ Number Palindrome Checker/palindrome.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int isPalindrome(int num) {
+    int reversedNum = 0;
+    int originalNum = num;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num /= 10;
+    }
+
+    if (originalNum == reversedNum) {
+        return 1; // Palindrome
+    } else {
+        return 0; // Not a palindrome
+    }
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPalindrome(num)) {
+        printf("%d is a palindrome.\n", num);
+    } else {
+        printf("%d is not a palindrome.\n", num);
+    }
+
+    return 0;
+}
 
