c00ae9b5134f09d4cab3c8c65ecb14c9bc0aa22b
Kalutu Daniel <kalutudaniel@gmail.com>
2023-06-22 22:52:01 +0300

    Update multiplicationtable.c

--- Multiplication Table/multiplicationtable.c
+++ Multiplication Table/multiplicationtable.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int rows, columns, i, j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &columns);
+
+    printf("\nMultiplication Table:\n");
+
+    // Print column headers
+    printf("  ");
+    for (j = 1; j <= columns; j++) {
+        printf("%4d", j);
+    }
+    printf("\n");
+
+    // Print table rows
+    for (i = 1; i <= rows; i++) {
+        printf("%2d", i);
+        for (j = 1; j <= columns; j++) {
+            printf("%4d", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
